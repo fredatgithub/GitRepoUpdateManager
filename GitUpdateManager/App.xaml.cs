ca87f7ab459f20574d60bfa5a3a27e333ed49d7f
fred <domaine75@gmail.com>
2026-03-31 11:18:08 +0200

    Ajoutez des fichiers projet.

---
+++ GitUpdateManager/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace GitUpdateManager
+{
+  /// <summary>
+  /// Logique d'interaction pour App.xaml
+  /// </summary>
+  public partial class App: Application
+  {
+  }
+}
